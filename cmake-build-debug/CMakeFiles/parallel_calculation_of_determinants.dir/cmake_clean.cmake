a192cc1bf7dd3291199a8450aee7946be5475da0
Dmitriy_Utyuzhnikov <dima-utyuzhnikov2013@yandex.ru>
2021-07-01 22:02:26 +0300

    parallel-determinant

---
+++ cmake-build-debug/CMakeFiles/parallel_calculation_of_determinants.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallel_calculation_of_determinants.dir/main.cpp.o"
+  "libparallel_calculation_of_determinants.dll.a"
+  "parallel_calculation_of_determinants.exe"
+  "parallel_calculation_of_determinants.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parallel_calculation_of_determinants.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
